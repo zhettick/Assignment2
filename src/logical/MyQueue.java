8a8326c148a57f71c076c885c26327b739af92e6
Diana <dianasabulla@MacBook-Air-Diana.local>
2025-04-13 20:49:44 +0500

    MyQueue

--- src/logical/MyQueue.java
+++ src/logical/MyQueue.java
@@ -1,5 +1,33 @@
 package logical;
 
-public class MyQueue {
+import physical.MyLinkedList;
 
+public class MyQueue<T extends Comparable<T>> {
+    public MyLinkedList<T> list = new MyLinkedList<>();
+
+    public boolean empty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public T peek() {
+        if (empty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return list.getFirst();
+    }
+
+    public void enqueue(T item) {
+        list.add(item);
+    }
+
+    public void dequeue() {
+        if (empty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        list.removeFirst();
+    }
 }
