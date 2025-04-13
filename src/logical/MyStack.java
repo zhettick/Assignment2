5a27e2d0d39f0918ef2f4dd8d970bbf449860e70
Diana <dianasabulla@MacBook-Air-Diana.local>
2025-04-13 20:33:14 +0500

    MyStack

--- src/logical/MyStack.java
+++ src/logical/MyStack.java
@@ -1,5 +1,36 @@
 package logical;
 
-public class MyStack {
+import physical.MyArrayList;
 
+public class MyStack<T> {
+    public MyArrayList<T> list = new MyArrayList<>();
+
+    public boolean empty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public T peek() {
+        if (empty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.getLast();
+    }
+
+    public T push(T item) {
+        list.add(item);
+        return item;
+    }
+
+    public T pop() {
+        if (empty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T item = list.getLast();
+        list.removeLast();
+        return item;
+    }
 }
